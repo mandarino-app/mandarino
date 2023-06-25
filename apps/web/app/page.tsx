689ff05fd571957229652fe56aca65bda396e025
learnuidev <learnuidev@gmail.com>
2023-06-25 18:37:07 -0400

    fix build error

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,25 @@
-import { HomePage } from 'ui/pages'
+// import { HomePage } from 'ui/pages/home/v1'
+
+// export default HomePage
+
+'use client'
+// import { HomePage } from 'ui/pages'
+
+const HomePage = () => {
+  return (
+    <>
+      <h1> Mandarino </h1>
+      <p>mandarino helps you learn mandarin fast and fun</p>
+      <button
+        onClick={() => {
+          alert('mandarino')
+        }}
+        className=''
+      >
+        search
+      </button>
+    </>
+  )
+}
 
 export default HomePage
--- packages/ui/index.tsx
+++
@@ -1,3 +0,0 @@
-// component exports
-export * as Button from './Button'
-export * as Header from './Header'
