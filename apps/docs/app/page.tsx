1a9f96e995514e92f3fddd4fe28e2736524f4660
learnuidev <learnuidev@gmail.com>
2023-06-25 18:32:20 -0400

    fix error

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,4 +1,21 @@
 'use client'
-import { HomePage } from 'ui/pages'
+// import { HomePage } from 'ui/pages'
+
+const HomePage = () => {
+  return (
+    <>
+      <h1> Mandarino </h1>
+      <p>mandarino helps you learn mandarin fast and fun</p>
+      <button
+        onClick={() => {
+          alert('mandarino')
+        }}
+        className=''
+      >
+        search
+      </button>
+    </>
+  )
+}
 
 export default HomePage
